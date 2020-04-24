a6f1e8238d45d07e8a5cf2e136123c4539a0be7f
Marco Lam <lamma3@oocl.com>
2020-04-24 20:51:37 +0800

    [Marco] - test active profile

--- src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
+++ src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
@@ -1,7 +1,10 @@
 package com.oocl.easyparkbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EasyParkBackendApplication {
@@ -10,4 +13,11 @@ public class EasyParkBackendApplication {
 		SpringApplication.run(EasyParkBackendApplication.class, args);
 	}
 
+	@Value("${env}")
+	private String env;
+
+	@PostConstruct
+	public void test() {
+		System.out.println(env);
+	}
 }
