3c38d93e6a0090c7ea9c4663ab895b95fbe474a9
Dennis Chan <dennis.chan@oocl.com>
2020-04-25 19:43:20 +0800

    add space behind table name

--- src/main/resources/db/migration/V202004251906__Add_column_hour_rate_distance_rating_to_parking_lot_table.sql
+++ src/main/resources/db/migration/V202004251906__Add_column_hour_rate_distance_rating_to_parking_lot_table.sql
@@ -1,4 +1,4 @@
-ALTER TABLE parking_lot
+ALTER TABLE parking_lot 
 ADD COLUMN hour_rate double precision NOT NULL DEFAULT 0.0,
 ADD COLUMN distance double precision NOT NULL DEFAULT 0.0,
 ADD COLUMN rating double precision NOT NULL DEFAULT 0.0;
