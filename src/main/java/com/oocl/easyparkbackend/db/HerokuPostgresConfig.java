3d20a14e3380bae660803931fe31222eb79dc465
lokmingjay <jay.ngan@oocl.com>
2020-04-25 20:54:04 +0800

    [NGANJA] add postgre config back

--- src/main/java/com/oocl/easyparkbackend/db/HerokuPostgresConfig.java
+++ src/main/java/com/oocl/easyparkbackend/db/HerokuPostgresConfig.java
@@ -15,7 +15,7 @@ public class HerokuPostgresConfig {
     
     @Bean
     public DataSource dataSource() throws URISyntaxException {
-        //URI dbUri = new URI(System.getenv("postgres://ipiwgawjdcgplv:8960f31ff028963846d58cc55ef7bc18732cf75635dc5b4f0e150f3fdd4f81@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d6dmm9j60cskhf"));
+       // URI dbUri = new URI(System.getenv("postgres://ipiwgawjdcgplv:8960f31ff028963846d58cc55ef7bc18732cf75635dc5b4f0e150f3fdd4f81@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d6dmm9j60cskhf"));
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
