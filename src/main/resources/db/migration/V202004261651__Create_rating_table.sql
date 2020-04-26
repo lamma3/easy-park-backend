b44c18410dba6b665337995baf7cafa1ec5db9b4
Marco Lam <lamma3@oocl.com>
2020-04-26 19:41:29 +0800

    [Marco] fix db script for auto_increment

--- src/main/resources/db/migration/V202004261651__Create_rating_table.sql
+++ src/main/resources/db/migration/V202004261651__Create_rating_table.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS rating;
 CREATE TABLE rating (
- id int auto_increment primary key,
+ id serial,
  score double not null,
  parking_lot_id int,
  foreign key (parking_lot_id) references parking_lot(id)
