39220d96927dbd5338e8a24716eb9d654c8f1acd
Dennis Chan <dennis.chan@oocl.com>
2020-04-27 10:58:24 +0800

    [Dennis] findAllParkingLot bug fix

--- src/main/java/com/oocl/easyparkbackend/service/ParkingLotService.java
+++ src/main/java/com/oocl/easyparkbackend/service/ParkingLotService.java
@@ -4,30 +4,48 @@ import com.oocl.easyparkbackend.model.ParkingLot;
 import com.oocl.easyparkbackend.repository.ParkingLotRepository;
 import com.oocl.easyparkbackend.repository.ParkingLotRepositoryCustomImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import javax.persistence.EntityManager;
+import java.util.Comparator;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 public class ParkingLotService {
+
+    private static String ASC_ORDER = "asc";
 
     @Autowired
     private ParkingLotRepository repository;
 
-    @Autowired
-    private ParkingLotRepositoryCustomImpl customRepository;
-
     public List<ParkingLot> findAll(Double priceFrom, Double priceTo, Double maxDistance, String ratingOrder) {
-        return customRepository.findAllParkingLotsByFilterCondition(priceFrom, priceTo, maxDistance, ratingOrder);
+        List<ParkingLot> parkingLotList = repository.findAll();
+
+        Stream<ParkingLot> parkingLotStream = parkingLotList.stream();
+        if (priceFrom != null){
+            parkingLotStream = parkingLotStream.filter(parkingLot -> priceFrom <= parkingLot.getHourRate());
+        }
+
+        if (priceTo != null){
+            parkingLotStream = parkingLotStream.filter(parkingLot -> priceTo >= parkingLot.getHourRate());
+        }
+
+        if(maxDistance != null){
+            parkingLotStream = parkingLotStream.filter(parkingLot -> maxDistance >= parkingLot.getDistance());
+        }
+
+        List<ParkingLot> resultList = parkingLotStream.collect(Collectors.toList());
+
+        if(ASC_ORDER.equals(ratingOrder)){
+            resultList.sort(Comparator.comparingDouble(ParkingLot::getRating));
+        }else {
+            resultList.sort(Comparator.comparingDouble(ParkingLot::getRating).reversed());
+        }
+
+        return resultList;
     }
 
     public ParkingLot findParkingLotById(Integer parkingLotId) {
