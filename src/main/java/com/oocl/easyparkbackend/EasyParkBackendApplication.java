f9c80b539ba4504fb8bcc5def708dd10a15f1444
Marco Lam <lamma3@oocl.com>
2020-04-28 12:47:31 +0800

    [Marco] add swagger ui

--- src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
+++ src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
@@ -2,12 +2,26 @@ package com.oocl.easyparkbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EasyParkBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyParkBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
