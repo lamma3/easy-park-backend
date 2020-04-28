d1f05a93e2504fdc40a0dcd56e2a978217fbdda6
Marco Lam <lamma3@oocl.com>
2020-04-28 12:58:09 +0800

    [Marco] fix cors url

--- src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
+++ src/main/java/com/oocl/easyparkbackend/EasyParkBackendApplication.java
@@ -1,5 +1,6 @@
 package com.oocl.easyparkbackend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EasyParkBackendApplication {
+
+	@Value("${env.frontend.url}")
+	private String frontendUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyParkBackendApplication.class, args);
@@ -19,7 +23,7 @@ public class EasyParkBackendApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000");
+						.allowedOrigins(frontendUrl);
 			}
 		};
 	}
